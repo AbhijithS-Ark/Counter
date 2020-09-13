9a56292570e0b3a075d0729c7b58d11bf2cfd9c9
AbhijithS-Ark <abhijiths5303@gmail.com>
2020-09-13 18:12:56 +0530

    javascript counter

---
+++ app.js
@@ -0,0 +1,35 @@
+let counter = document.querySelector('.counter');
+const addCount = document.querySelector('.addbtn')
+const lowerCount = document.querySelector('.lowerbtn')
+
+
+let count=0;
+
+addCount.addEventListener('click',incrementcounter);
+lowerCount.addEventListener('click',decrementcounter);
+
+ function incrementcounter (){
+     count++;
+     counter.innerHTML = count;
+     if(counter.innerHTML >'0'){
+         counter.style.color ='#63F700';
+     }
+     else if(counter.innerHTML ==='0'){
+         counter.style.color='white';
+     }
+     counter.animate([{opacity:'0.2'},{opacity:'1.0'}],{duration:500,fill:'forwards'})
+ } 
+
+ 
+ function decrementcounter (){
+    count--;
+    counter.innerHTML = count;
+    if(counter.innerHTML <'0'){
+     counter.style.color='red';
+    }
+     else if(counter.innerHTML ==='0'){
+        counter.style.color='white';
+    }
+    counter.animate([{opacity:'0.2'},{opacity:'1.0'}],{duration:500,fill:'forwards'})
+
+}
