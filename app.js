1f861582d0970f6fb6dc5a51ba9406a08bac83e5
AbhijithS-Ark <abhijiths5303@gmail.com>
2020-09-13 19:22:29 +0530

    added reset button to counter

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 let counter = document.querySelector('.counter');
 const addCount = document.querySelector('.addbtn')
 const lowerCount = document.querySelector('.lowerbtn')
-
+const reset = document.querySelector('.reset')
 
 let count=0;
 
 addCount.addEventListener('click',incrementcounter);
 lowerCount.addEventListener('click',decrementcounter);
+reset.addEventListener('click',resetcall);
 
  function incrementcounter (){
      count++;
@@ -33,3 +34,7 @@ lowerCount.addEventListener('click',decrementcounter);
     counter.animate([{opacity:'0.2'},{opacity:'1.0'}],{duration:500,fill:'forwards'})
 
 }
+ function resetcall (){
+       counter.innerHTML = '0';
+       counter.style.color = 'white';
+    }
